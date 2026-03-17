4649aac514805cb9b8a3a5b5563d21fcfc5c29f8
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2026-03-17 18:54:30 +0530

    Revert "Reapply "Clean Arch""
    This reverts commit 841f8f02b532ec0d6722f5b3c3a21871e55cb901.

---
+++ ProperyWrappersBC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProperyWrappersBC
+//
+//  Created by G Yacobu on 16/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
